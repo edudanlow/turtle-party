5879b590fb58d58361c93b0affcb85809654a5ba
edudanlow <162932531+edudanlow@users.noreply.github.com>
2024-03-10 20:50:49 -0400

    Create main.py
    Fun with turtle creating  squares and circles

---
+++ main.py
@@ -0,0 +1,19 @@
+#Turtle Project by Eduardo Lowensohn
+#03/10/2024
+
+import turtle
+turtle.color("red")
+
+def back(len):
+  turtle.penup()
+  turtle.backward(len)
+  turtle.pendown()
+  
+def polygon(num, size):
+  for i in range(num):
+    turtle.forward(size)
+    turtle.left(360 / num)
+    
+polygon(4, 100)
+back(125)
+polygon(3, 50)
